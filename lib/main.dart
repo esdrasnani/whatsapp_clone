3c2d8ef696de57e6d32d3450782d3d0a7373e37a
Esdras Nani <esdraslnani@hotmail.com>
2020-01-14 13:03:23 -0300

    Add Time

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
 
   TabController tabController;
   IconData fabIcon;
+  ScrollController scrollViewController = new ScrollController();
+
   @override
   void initState() {
     super.initState();
@@ -51,40 +53,53 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
   Widget build(BuildContext context) {
     return 
         Scaffold(
-          appBar: AppBar(
-            title: Text("WhatsApp", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
-            backgroundColor: Color(0xff075E55),
-            actions: <Widget>[              
-                IconButton(
-                  icon: Icon(Icons.search, size: 25),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.more_vert, size: 23),
-                  onPressed: () {},
-                )
-            ],
-            bottom: 
-              TabBar(
+          body: 
+            NestedScrollView(
+              controller: scrollViewController,
+              headerSliverBuilder: (context, innerBoxIsScrolled){
+                return <Widget>[
+                  SliverAppBar(
+                    title: Text("WhatsApp", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
+                    pinned: true,      
+                    floating: true,
+                    forceElevated: innerBoxIsScrolled,
+                    backgroundColor: Color(0xff075E55),
+                    actions: <Widget>[              
+                        IconButton(
+                          icon: Icon(Icons.search, size: 25),
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.more_vert, size: 23),
+                          onPressed: () {},
+                        )
+                    ],
+                    bottom: 
+                      TabBar(
+                        controller: tabController,
+                        tabs: [
+                            Tab(icon: Icon(Icons.camera_alt)),
+                            Tab(text: "CHATS"),
+                            Tab(text: "STATUS"),
+                            Tab(text: "CALLS"),
+                        ],
+                        indicatorColor: Colors.white,     
+                      ),
+                  ),
+                ];
+              },
+              
+            body: TabBarView(
                 controller: tabController,
-                tabs: [
-                    Tab(icon: Icon(Icons.camera_alt)),
-                    Tab(text: "CHATS"),
-                    Tab(text: "STATUS"),
-                    Tab(text: "CALLS"),
+                children: [
+                  Icon(Icons.camera_alt),
+                  ChatList(),
+                  Text("STATUS SCREEN"),
+                  Text("CALL SCREEN"),
                 ],
-                indicatorColor: Colors.white,
               ),
-          ),
-          body: TabBarView(
-              controller: tabController,
-              children: [
-                Icon(Icons.camera_alt),
-                ChatList(),
-                Text("STATUS SCREEN"),
-                Text("CALL SCREEN"),
-              ],
             ),
+
           floatingActionButton: FloatingActionButton(
             onPressed: () {},
             child: Icon(fabIcon),
@@ -97,21 +112,51 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
 class ChatList extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-      return ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return 
-              Column(
-                children: <Widget>[
-                  Row(
+      return Padding(
+        padding: EdgeInsets.only(bottom: 0),  
+        child:
+          ListView.builder(
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return 
+                  Column(
                     children: <Widget>[
-                      Icon(Icons.account_circle, size: 64)
+                      Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(left: 5),
+                            child: Icon(Icons.account_circle, size: 60)
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.only(left: 5),
+                                child: 
+                                  Row(children: <Widget>[
+                                    Text("Amor ♥", style: TextStyle(fontWeight: FontWeight.w500 ,fontSize: 18), textAlign: TextAlign.left,),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 250),
+                                      child: Text("10:50", style: TextStyle(color: Colors.grey, fontSize: 14))
+                                    )
+                                  ],)
+                                
+                              ),
+                              
+                              Padding(
+                                padding: EdgeInsets.only(right: 220, top: 5),
+                                child: Text("A ultima mensagem", style: TextStyle(color: Colors.grey, fontSize: 15),)
+                              ),
+
+                              
+                            ],
+                          )
+                        ],
+                      ),
+                      Divider()
                     ],
-                  ),
-                  Divider()
-                ],
-              );
-        },
+                  );
+            },
+          )
       );
     }
   }
